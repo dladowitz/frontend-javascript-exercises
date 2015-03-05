18385ce38a0c2e5ad6a6c7c1f8a2ffd4c58aa540
David Ladowitz <david@ladowitz.com>
2015-03-05 23:21:45 +0000

    objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,29 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students){
+  return { title: title, duration: duration, students: students };
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, property, value){
+  if (object[property] === undefined){
+    object[property] = value;
+  } 
+  return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(product, money){
+  var inventory = {
+    "MacBook Air": 999,
+    "MacBook Pro": 1299,
+    "Mac Pro": 2499,
+    "Apple Sticker": 1,
+  }
+
+  if(money >= inventory[product]){
+    return true;
+  } else {
+    return false;
+  }
+};
